5a37b8ed567145962f1db32d43c38dd896e3c3c1
Maksym Solomko <max.exc7@gmail.com>
2023-03-03 22:53:55 +0100

    REST_API

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,14 +1,34 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Heading from "../../components/Heading";
 
 
-const Contacts = () => (
+const Contacts = () => {
+    const [contacts, setContacts] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const responce = await fetch('https://jsonplaceholder.typicode.com/users/');
+            const data = await responce.json();
+            setContacts(data);
+        }
+        fetchData();
+    }, []);
+    
+
+    return (
     <>
         <Head>
             <title>Contacts</title>
         </Head>
         <Heading text="Contacts list:" />
+        <ul>
+            {contacts && contacts.map(({id, name, email}) => (
+                <li key={id}><strong>{name}</strong> ({email})</li>
+            ))}
+        </ul>
     </>
 );
+}
 
 export default Contacts;
