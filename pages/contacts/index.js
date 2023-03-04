f0e4e8bb2e46050fb15222b03ca519f9d5eefaa7
Maksym Solomko <max.exc7@gmail.com>
2023-03-04 12:18:22 +0100

    Update index.js

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -8,8 +8,8 @@ const Contacts = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const responce = await fetch('https://jsonplaceholder.typicode.com/users/');
-            const data = await responce.json();
+            const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+            const data = await response.json();
             setContacts(data);
         }
         fetchData();
