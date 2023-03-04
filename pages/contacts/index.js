9b9907766ce4dbd2907b6feb4441765ab0f2f2a9
Maksym Solomko <max.exc7@gmail.com>
2023-03-04 12:28:34 +0100

    getStaticPropsgetStaticProps

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,19 +1,30 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Heading from "../../components/Heading";
 
+export const getStaticProps = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+        const data = await response.json();
 
-const Contacts = () => {
-    const [contacts, setContacts] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users/');
-            const data = await response.json();
-            setContacts(data);
+        return {
+            props: { contacts: data },
         }
-        fetchData();
-    }, []);
+}
+
+
+const Contacts = ({ contacts }) => {
+    //  ----- we will use the server -----
+    
+    // const [contacts, setContacts] = useState(null);
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+    //         const data = await response.json();
+    //         setContacts(data);
+    //     }
+    //     fetchData();
+    // }, []);
     
 
     return (
