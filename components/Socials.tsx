8adc53ab9406daa82702d823b459f9c113a23970
Maksym Solomko <max.exc7@gmail.com>
2023-03-05 17:32:55 +0100

    Update Socials.tsx

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -20,7 +20,7 @@ const Socials:FC<socialInfoType> = ({ socials }) => {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/css/all.css" />
     </Head>
     <ul className={styles.socials}>
-      {socials && socials.map(({ id, icon, path }) => (
+      { socials.map(({ id, icon, path }) => (
         <li key={id} title={icon}>
           <a href={path} target="_blank" rel="noopener noreferrer" >
             <i className={`fab fa-${icon}`} aria-hidden="true"/>
