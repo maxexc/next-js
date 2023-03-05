aa9dabc265a07ff0eb66f05b0a12ae99a30b4e18
Maksym Solomko <max.exc7@gmail.com>
2023-03-05 21:46:57 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env:{
-  //   API_HOST : process.env.API_HOST,
-  // }
+  env:{
+    API_HOST: process.env.API_HOST,
+  }
 }
 
 module.exports = nextConfig
