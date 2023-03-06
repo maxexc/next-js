5834da54f13060ad88ccfcd65b26f3ec96f8c865
Maksym Solomko <max.exc7@gmail.com>
2023-03-06 13:23:16 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,94 @@
-// import { GetStaticProps } from "next";
-import { FC } from "react";
-import { socialType } from "../types";
 import Head from "next/head";
 import Heading from "../components/Heading";
 import Socials from "../components/Socials";
-import styles from "../styles/Home.module.scss"
-
-type socialInfoType = {
-  socials: [socialType],
-}
-
-console.log("test");
+import styles from "../styles/Home.module.scss";
 
 console.log(process.env.NEXT_PUBLIC_API_HOST);
 
-export const getServerSideProps = async () => {
-// export const getStaticProps:GetStaticProps = async () => {
+
+export const getStaticProps = async () => {
   try {
-  const response = await fetch("https://next-js-nine-teal.vercel.app/api/socials/");
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/socials/`);
-  const data = await response.json();
-
-  if (!data) {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/socials`);
+    const data = await response.json();
+  
+    if (!data) {
       return {
-          notFound: true,
+        notFound: true,
       }
-  }
-
-  return {
+    }
+  
+    return {
       props: { socials: data },
+    }
+  } catch {
+    return {
+      props: { socials: null },
+    }
   }
-}
- catch {
-  return {
-    props: {socials: null},
-  }
-}
 };
 
-const Home:FC<socialInfoType> = ({ socials }) => (
+const Home = ({ socials }) => (
   <div className={styles.wrapper}>
     <Head>
-        <title>Home</title>
+      <title>Home</title>
     </Head>
-    <Heading text="Next.js Application" /> 
+    <Heading text="Next.js Application" />
     <Socials socials={socials} />
   </div>
 );
 
 export default Home;
+
+
+
+// import { GetStaticProps } from "next";
+// import { FC } from "react";
+// import { socialType } from "../types";
+// import Head from "next/head";
+// import Heading from "../components/Heading";
+// import Socials from "../components/Socials";
+// import styles from "../styles/Home.module.scss"
+
+// type socialInfoType = {
+//   socials: [socialType],
+// }
+
+// console.log("test");
+
+// console.log(process.env.NEXT_PUBLIC_API_HOST);
+
+// export const getServerSideProps = async () => {
+// // export const getStaticProps:GetStaticProps = async () => {
+//   try {
+//   const response = await fetch("https://next-js-nine-teal.vercel.app/api/socials/");
+//   // const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/socials/`);
+//   const data = await response.json();
+
+//   if (!data) {
+//       return {
+//           notFound: true,
+//       }
+//   }
+
+//   return {
+//       props: { socials: data },
+//   }
+// }
+//  catch {
+//   return {
+//     props: {socials: null},
+//   }
+// }
+// };
+
+// const Home:FC<socialInfoType> = ({ socials }) => (
+//   <div className={styles.wrapper}>
+//     <Head>
+//         <title>Home</title>
+//     </Head>
+//     <Heading text="Next.js Application" /> 
+//     <Socials socials={socials} />
+//   </div>
+// );
+
+// export default Home;
